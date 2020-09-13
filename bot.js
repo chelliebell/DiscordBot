8345b6844703242072c49d827502935a2fb6d5a3
chelliebell <46589370+chelliebell@users.noreply.github.com>
2020-09-13 18:28:51 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,5 @@
-const Discord = require('discord.js');
 
-const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.MLaiB6WlUjv7TwNVmlq-e9vGrC1A_jz6);//BMLaiB6WlUjv7TwNVmlq-e9vGrC1A_jz6 is the Client Secret
 /******************************************************
  * Discord Bot Maker Bot
  * Version 1.6.10
@@ -1875,3 +1848,6 @@ Structures.extend("GuildEmoji", (GuildEmoji) => class extends GuildEmoji {
 //---------------------------------------------------------------------
 
 Files.startBot();
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.MLaiB6WlUjv7TwNVmlq-e9vGrC1A_jz6);//BMLaiB6WlUjv7TwNVmlq-e9vGrC1A_jz6 is the Client Secret
